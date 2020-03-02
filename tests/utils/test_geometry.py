08a8072550d3adc720383fd4e14e8338d8b076b2
Ki-Jana Carter <kijana@mit.edu>
2020-03-02 11:44:25 -0500

    Add tests to reflect new utils file structure

--- tests/utils/test_geometry.py
+++ tests/utils/test_geometry.py
@@ -1,6 +1,6 @@
 import numpy as np
 
-from materia.utils.geometry import *
+import materia as mtr
 
 yz_swap = np.array([[1, 0, 0], [0, 0, 1], [0, 1, 0]])
 xy_plane_reflection = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -1]])
@@ -9,36 +9,40 @@ xy_plane_reflection = np.array([[1, 0, 0], [0, 1, 0], [0, 0, -1]])
 def geometry_perpendicular_vector():
     # FIXME: expand
     assert (
-        perpendicular_vector(a=np.array([[1, 0, 0]]).T) == np.array([[0, 1, 0]]).T
+        mtr.perpendicular_vector(a=np.array([[1, 0, 0]]).T) == np.array([[0, 1, 0]]).T
     ).all()
 
 
 def test_geometry_rotation_matrix_m_to_n():
     assert (
-        rotation_matrix_m_to_n(m=np.array([[1.0, 0, 0]]).T, n=np.array([[1.0, 0, 0]]).T)
+        mtr.rotation_matrix_m_to_n(
+            m=np.array([[1.0, 0, 0]]).T, n=np.array([[1.0, 0, 0]]).T
+        )
         == np.eye(3)
     ).all()
     assert (
-        rotation_matrix_m_to_n(m=np.array([[1.0, 0, 0]]).T, n=np.array([[0, 1.0, 0]]).T)
+        mtr.rotation_matrix_m_to_n(
+            m=np.array([[1.0, 0, 0]]).T, n=np.array([[0, 1.0, 0]]).T
+        )
         == np.array([[0, -1, 0], [1, 0, 0], [0, 0, 1]])
     ).all()
 
 
 def test_geometry_nontrivial_vector():
-    v = nontrivial_vector(R=yz_swap, seed=42809437)
+    v = mtr.nontrivial_vector(R=yz_swap, seed=42809437)
     assert not (yz_swap @ v == v).all()
 
-    v = nontrivial_vector(R=xy_plane_reflection, seed=42809437)
+    v = mtr.nontrivial_vector(R=xy_plane_reflection, seed=42809437)
     assert not (xy_plane_reflection @ v == v).all()
 
-    v = nontrivial_vector(R=yz_swap, seed=139856018)
+    v = mtr.nontrivial_vector(R=yz_swap, seed=139856018)
     assert not (yz_swap @ v == v).all()
 
-    v = nontrivial_vector(R=xy_plane_reflection, seed=139856018)
+    v = mtr.nontrivial_vector(R=xy_plane_reflection, seed=139856018)
     assert not (xy_plane_reflection @ v == v).all()
 
 
 def test_geometry_closest_trio():
-    # !FIXME: expand
+    # FIXME: expand
     points = np.array([[1, 0, 0], [0, 0, 0], [1e-4, 0, 0], [1e-3, 0, 0]]).T
-    assert (closest_trio(points=points) == points[:, 1:]).all()
+    assert (mtr.closest_trio(points=points) == points[:, 1:]).all()
--- tests/utils/test_maxlipotr_minimizer.py
+++ tests/utils/test_maxlipotr_minimizer.py
@@ -1,12 +1,32 @@
+import materia as mtr
 import numpy as np
-
-import materia.utils
-
 import pytest
 
 
+def test_gss_bracket_delta_01():
+    s = mtr.GoldenSectionSearch(objective_function=lambda x: (x - 2) ** 2)
+
+    assert s._find_gss_bracket(delta=0.01) == (1.2137383539249436, 3.2037886039123507)
+
+
+def test_gss_bracket_delta_02():
+    s = mtr.GoldenSectionSearch(objective_function=lambda x: (x - 2) ** 2)
+
+    assert s._find_gss_bracket(delta=0.02) == (1.487902432574931, 3.947739820199816)
+
+
+def test_gss_optimize_delta_01_tol_0001():
+    s = mtr.GoldenSectionSearch(objective_function=lambda x: (x - 2) ** 2)
+
+    test_result = tuple(s.optimize(delta=0.01, tolerance=0.0001).values())
+    print(test_result)
+    check_result = (9.646599407882878e-10, 25, 1.999968941024795, 20, True)
+
+    assert test_result == check_result
+
+
 def test_maxlipotr_optimize_min_0_max_5_numevals_20():
-    s = materia.utils.MaxLIPOTRMinimizer(objective_function=lambda x: (x - 2) ** 2)
+    s = mtr.MaxLIPOTR(objective_function=lambda x: (x - 2) ** 2)
 
     assert s.optimize(x_min=0, x_max=5, num_evals=20) == ([2.0], 0.0)
 
@@ -18,7 +38,7 @@ def test_maxlipotr_optimize_rastrigin():
     def rastigrin(x):
         return 10 * len(xs) + np.sum(xs - 10 * np.cos(2 * np.pi * xs))
 
-    s = materia.utils.MaxLIPOTRMinimizer(objective_function=rastigrin)
+    s = mtr.MaxLIPOTR(objective_function=rastigrin)
 
     test_result = tuple(s.optimize(x_min=-5.12, x_max=5.12, num_evals=10))
     check_result = ([0.0], 75.99791446043284)
@@ -35,7 +55,7 @@ def test_maxlipotr_optimize_ackley():
             + 20
         )
 
-    s = materia.utils.MaxLIPOTRMinimizer(objective_function=ackley)
+    s = mtr.MaxLIPOTR(objective_function=ackley)
 
     test_result = tuple(s.optimize(x_min=[-5, -5], x_max=[5, 5], num_evals=10))
     check_result = ([0.0, 0.0], 0.0)
@@ -51,7 +71,7 @@ def test_maxlipotr_optimize_beale():
             + (2.625 - x + x * y ** 3) ** 2
         )
 
-    s = materia.utils.MaxLIPOTRMinimizer(objective_function=beale)
+    s = mtr.MaxLIPOTR(objective_function=beale)
 
     test_result = tuple(s.optimize(x_min=[-4.5, -4.5], x_max=[4.5, 4.5], num_evals=300))
     check_result = ([2.9999999999998272, 0.5000000000001019], 4.880287250592691e-25)
