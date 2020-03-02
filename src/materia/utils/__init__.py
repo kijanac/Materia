4a9838470918aecb7bc05264424f50c5fb325a56
Ki-Jana Carter <kijana@mit.edu>
2020-03-02 11:42:14 -0500

    Consolidate most utils code into a single file.

--- src/materia/utils/__init__.py
+++ src/materia/utils/__init__.py
@@ -1,14 +1,4 @@
 from .dataseries import *
-from .expand import *
-from .extrapolate import extrapolate
-from .geometry import *
-from .interpolate import interpolate
-from .math import *
-from .memoize import memoize
-from .minimizer import *
-from .nested_dictionary import *
-from .settings import *
-from .temporary_seed import temporary_seed
-from .work_dir import *
+from ._utils import *
 
 from .xyz2mol import xyz2molUSE
--- src/materia/utils/minimizer/__init__.py
+++
@@ -1,2 +0,0 @@
-from .gss import *
-from .maxlipotr import *
