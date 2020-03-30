b753754ad34efca8a752daf64927484774ebf34b
Ki-Jana Carter <kijana@mit.edu>
2020-03-30 12:47:30 -0400

    Update GSS tests to match GoldenSectionSearch hotfix

--- tests/utils/test_maxlipotr_minimizer.py
+++ tests/utils/test_maxlipotr_minimizer.py
@@ -6,13 +6,13 @@ import pytest
 def test_gss_bracket_delta_01():
     s = mtr.GoldenSectionSearch(objective_function=lambda x: (x - 2) ** 2)
 
-    assert s._find_gss_bracket(delta=0.01) == (1.2137383539249436, 3.2037886039123507)
+    assert s._find_gss_bracket(delta=0.01) == (1.0933645185324488, 2.8844097435211147)
 
 
 def test_gss_bracket_delta_02():
     s = mtr.GoldenSectionSearch(objective_function=lambda x: (x - 2) ** 2)
 
-    assert s._find_gss_bracket(delta=0.02) == (1.487902432574931, 3.947739820199816)
+    assert s._find_gss_bracket(delta=0.02) == (1.414507310946184, 3.751352829189824)
 
 
 def test_gss_optimize_delta_01_tol_0001():
@@ -20,7 +20,7 @@ def test_gss_optimize_delta_01_tol_0001():
 
     test_result = tuple(s.optimize(delta=0.01, tolerance=0.0001).values())
     print(test_result)
-    check_result = (9.646599407882878e-10, 25, 1.999968941024795, 20, True)
+    check_result = (6.17113206775526e-10, 25, 1.9999751582366412, 20, True)
 
     assert test_result == check_result
 
