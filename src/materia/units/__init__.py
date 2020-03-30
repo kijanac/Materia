68bfa6fc719cb530e44dd86a0d96700b099f9b2d
Ki-Jana Carter <kijana@mit.edu>
2020-03-30 12:37:16 -0400

    Delete untis and utils folders

--- src/materia/units/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dimension import *
-from .unit import *
-from .units import *
-from .qty import *
--- src/materia/utils/__init__.py
+++
@@ -1,4 +0,0 @@
-from .dataseries import *
-from ._utils import *
-
-from .xyz2mol import xyz2molUSE
